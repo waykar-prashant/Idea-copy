b0d20176ad6f744dbbde6b84132b5c98f45850a4
ayushchoukse1 <ayush.choukse@sjsu.edu>
2015-10-25 16:50:32 -0700

    forecastAPI code

---
+++ ActiveDataCollection/src/com/idea/adapters/weather/forecastio/service/ForecastIOService.java
@@ -0,0 +1,15 @@
+package com.idea.adapters.weather.forecastio.service;
+
+public class ForecastIOService {
+
+	public final String API_KEY = "338ca7cbf5989fdad923f81b998255d3";
+	public final String BASE_URL = "https://api.forecast.io/forecast/";
+
+	public String getWeather(float longitute, float latitude){
+		String weather="";
+		
+		
+		
+		return weather;
+	}
+}
