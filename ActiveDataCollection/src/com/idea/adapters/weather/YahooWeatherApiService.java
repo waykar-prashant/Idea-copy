d635af29327e3e435d111e09ee0eb1dea825b31d
Ayush Choukse <ayush@midoplay.com>
2015-10-25 01:25:09 -0700

    Read the data from Url print it

--- ActiveDataCollection/src/com/idea/adapters/weather/YahooWeatherApiService.java
+++ ActiveDataCollection/src/com/idea/adapters/weather/YahooWeatherApiService.java
@@ -1,12 +1,22 @@
 package com.idea.adapters.weather;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.net.URL;
+import java.net.URLConnection;
+
 public class YahooWeatherApiService {
 
 	/*
 	 * public String baseUrl = "http://weather.yahooapis.com/forecastrss";
 	 * public String woeidParameter = "w"; public String degreeParameter = "u";
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		String baseUrl = "http://weather.yahooapis.com/forecastrss";
 		String woeidParameter = "w";
 		String degreeParameter = "u";
@@ -18,10 +28,23 @@ public class YahooWeatherApiService {
 		 * 20geo.places(1)%20where%20text%3D%22nome%2C%20ak%22)&format=json&env=
 		 * store%3A%2F%2Fdatatables.org%2Falltableswithkeys
 		 */
-		StringBuilder url = new StringBuilder(baseUrl);
-		url.append("?").append(woeidParameter).append("=").append("2502265").append("&").append(degreeParameter)
+		StringBuilder urlString = new StringBuilder(baseUrl);
+		urlString.append("?").append(woeidParameter).append("=").append("2502265").append("&").append(degreeParameter)
 				.append("=").append("c");
-		System.out.println(url);
+		URL url = new URL(urlString.toString());
+		URLConnection connection = url.openConnection();
+		InputStream is = connection.getInputStream();
+		BufferedReader urlReader = new BufferedReader(new InputStreamReader(is));
+		StringWriter writer = new StringWriter();
+		String s="";
+		while ((s = urlReader.readLine()) != null) {
+		    writer.write(s);
+		}
+		//copy(reader, writer);
+		writer.close();
+		is.close();
+		System.out.println(writer.toString());
+		
 	}
 
 }
