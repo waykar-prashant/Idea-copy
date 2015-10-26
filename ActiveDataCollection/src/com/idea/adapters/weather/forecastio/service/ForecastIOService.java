0e27f60ecc23841953a16e5833c0ec0dda38afcb
ayushchoukse1 <ayush.choukse@sjsu.edu>
2015-10-25 17:42:05 -0700

    ForecaseIOService modularized

--- ActiveDataCollection/src/com/idea/adapters/weather/forecastio/service/ForecastIOService.java
+++ ActiveDataCollection/src/com/idea/adapters/weather/forecastio/service/ForecastIOService.java
@@ -6,39 +6,52 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.StringWriter;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 
 public class ForecastIOService {
 
-	public static final String API_KEY = "338ca7cbf5989fdad923f81b998255d3/";
+	public static final String API_KEY = "338ca7cbf5989fdad923f81b998255d3";
 	public static final String BASE_URL = "https://api.forecast.io/forecast/";
+	public static final String REQUEST_TYPE = "GET";
 
-	public static void main (String[] args) throws IOException {
-		String weather = "";
-		/*
-		 * Sample url
-		 * https://api.forecast.io/forecast/338ca7cbf5989fdad923f81b998255d3/37.8267,-122.423
-		 */
+	protected static String urlStringBuilder(double latitude, double longitude) {
 		StringBuilder urlString = new StringBuilder(BASE_URL);
-		urlString.append(API_KEY).append("37.8267,-122.423");
-		URL url = new URL(urlString.toString());
-		System.out.println(url.toString());
-		HttpURLConnection con = (HttpURLConnection)url.openConnection();
-		con.setRequestMethod("GET");
-		//URLConnection connection = url.openConnection();
+		urlString.append(API_KEY).append("/").append(latitude).append(",").append(longitude);
+		return urlString.toString();
+
+	}
+
+	protected static HttpURLConnection connectionBuilder(String urlString) throws IOException {
+		URL url = new URL(urlString);
+		return (HttpURLConnection) url.openConnection();
+	}
+
+	protected static String getRequestedCode(HttpURLConnection con) throws IOException {
 		InputStream is = con.getInputStream();
 		BufferedReader urlReader = new BufferedReader(new InputStreamReader(is));
 		StringWriter writer = new StringWriter();
-		String s="";
+		String s = "";
 		while ((s = urlReader.readLine()) != null) {
-		    writer.write(s);
+			writer.write(s);
 		}
-		//copy(reader, writer);
 		writer.close();
 		is.close();
-		System.out.println(writer.toString());
-		
-		
+		return writer.toString();
+	}
+
+	public static void main(String[] args) throws IOException {
+		String weather = "";
+		double latitude = 37.8267;
+		double longitude = -122.423;
+		/*
+		 * Sample url
+		 * https://api.forecast.io/forecast/338ca7cbf5989fdad923f81b998255d3/37.
+		 * 8267,-122.423
+		 */
+
+		String Response = getRequestedCode(connectionBuilder(urlStringBuilder(latitude, longitude)));
+		System.out.println(Response);
 	}
 }
